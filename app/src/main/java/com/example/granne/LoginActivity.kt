c6f5f47529d7bb74bc04da6ce9ac9adeacdbd7fc
Mattias <Mattias170624@outlook.com>
2021-10-12 14:49:52 +0200

    Added autologin switch

--- app/src/main/java/com/example/granne/LoginActivity.kt
+++ app/src/main/java/com/example/granne/LoginActivity.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.Switch
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var usernameEditText: EditText
     lateinit var passwordEditText: EditText
+    lateinit var autoLoginSwitch: Switch
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,13 +19,16 @@ class LoginActivity : AppCompatActivity() {
 
         usernameEditText = findViewById(R.id.usernameEditText)
         passwordEditText = findViewById(R.id.passwordEditText)
+        autoLoginSwitch = findViewById(R.id.autoLoginSwitch)
 
     }
 
     fun startMainScreenActivity(view: View) {
-        Log.d("!Login", "Login button pressed")
+        Log.d("!Login", "Login button pressed. Autologin = ${autoLoginSwitch.isChecked}")
 
         // här ska man först kolla om username och password matchar någon användare i databasen
         // sen om det username + password matchar med någon, så ska man skickas till Mainscreen
+        //
+        // Sen ska informationen om switch (true eller false) uppdateras i databasen
     }
 }
