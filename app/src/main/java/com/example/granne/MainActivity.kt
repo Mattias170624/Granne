f5b1213e825f2c165b8a6905b080afea70ca543d
Mattias <Mattias170624@outlook.com>
2021-10-12 15:31:17 +0200

    Merged

--- app/src/main/java/com/example/granne/MainActivity.kt
+++ app/src/main/java/com/example/granne/MainActivity.kt
@@ -6,31 +6,11 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 
-import android.widget.Button
-
-
-
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        val btnLogin = findViewById<Button>(R.id.btnLogin)
-        btnLogin.setOnClickListener {
-        }
-        val btnCreateAccount = findViewById<Button>(R.id.btnCreateAccount)
-        btnCreateAccount.setOnClickListener {
-
-
-            //start
-
-
-        }
-
-        //start
 
 
     }
