338ffac2ab7c87a5c0296690f2723c34c57009c9
fahirpepelar <fahir.pepelar@iths.se>
2021-10-12 15:16:11 +0200

    First screen (login / create account).

--- app/src/main/java/com/example/granne/MainActivity.kt
+++ app/src/main/java/com/example/granne/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.granne
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //start
-        var m: Int = 2
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+        }
+        val btnCreateAccount = findViewById<Button>(R.id.btnCreateAccount)
+        btnCreateAccount.setOnClickListener {
 
-        var e: Int = 4
 
-        var y: Int = 6
+            //start
+
+
+        }
 
 
     }
+
 }
