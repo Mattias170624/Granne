c7df28002309d31e82e5268fc451345b7d722b23
Mattias <Mattias170624@outlook.com>
2021-10-12 13:02:30 +0200

    Added login button and new activity

--- app/src/main/java/com/example/granne/MainActivity.kt
+++ app/src/main/java/com/example/granne/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.granne
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +14,11 @@ class MainActivity : AppCompatActivity() {
         //start
 
 
+    }
 
+    fun startLoginActivity(view: View) {
+        Log.d("Main", "Pressed login button")
+        val loginIntent = Intent(this@MainActivity, LoginActivity::class.java)
+        startActivity(loginIntent)
     }
 }
