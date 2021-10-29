04343a0d92df414513ca5b1b839c6f79a6842d89
Mattias <Mattias170624@outlook.com>
2021-10-29 11:27:32 +0200

    Made changes to layout

--- app/src/main/java/com/example/granne/CreateAccountActivity.kt
+++ app/src/main/java/com/example/granne/CreateAccountActivity.kt
@@ -7,124 +7,109 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class CreateAccountActivity : AppCompatActivity() {
 
-    lateinit var createNicknameEditText: EditText
-    lateinit var createUsernameEditText: EditText
-    lateinit var createPasswordEditText: EditText
-    lateinit var createAboutMeEditText: EditText
-    lateinit var createPhoneNumberEditText: EditText
-    lateinit var createConfirmPhoneNumberEditText: EditText
-    private lateinit var createNewAccountButton: Button
-
-    lateinit var nicknameText: String
-    lateinit var usernameText: String
-    lateinit var passwordText: String
-    lateinit var aboutMeText: String
-    lateinit var phoneNumberText: String
-    lateinit var phoneCodeText: String
-
+    private lateinit var auth: FirebaseAuth
     val db = Firebase.firestore
+
+    lateinit var buttonRegister: Button
+    lateinit var nicknameEditText: EditText
+    lateinit var emailEditText: EditText
+    lateinit var passwordEditText: EditText
+
+    lateinit var nickname: String
+    lateinit var email: String
+    lateinit var password: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+        auth = Firebase.auth
 
-        createNicknameEditText = findViewById(R.id.createNicknameEditText)
-        createUsernameEditText = findViewById(R.id.createUsernameEditText)
-        createPasswordEditText = findViewById(R.id.createPasswordEditText)
-        createAboutMeEditText = findViewById(R.id.createAboutYourselfEditText)
-        createPhoneNumberEditText = findViewById(R.id.createPhoneNumberEditText)
-        createConfirmPhoneNumberEditText = findViewById(R.id.createConfirmPhoneNumberEditText)
-        createNewAccountButton = findViewById(R.id.createNewAccountButton)
+        buttonRegister = findViewById(R.id.buttonRegister)
+        nicknameEditText = findViewById(R.id.nicknameEditText)
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
 
-
-        createNewAccountButton.setOnClickListener {
+        buttonRegister.setOnClickListener {
             when {
                 checkUserInputs() -> {
-                    Log.d("!", "User inputs are good")
-                    checkDuplicateAccountInfo()
+                    if (password.length >= 6) {
+                        if (nickname.length >= 6) {
+                            createAccount(email, password, nickname)
+                        } else showToast("Nickname must be longer than 6 characters")
+                    } else showToast("Password must be longer than 6 characters")
+                }
 
-                }
-                !checkUserInputs() -> {
-                    Log.d("!", "Some texts are empty")
-                    showToast("Some fields are missing!")
-                }
+                !checkUserInputs() -> showToast("Empty inputs!")
             }
         }
+
     }
 
-    private fun checkDuplicateAccountInfo() {
-        db.collection("users")
-            .whereEqualTo("nickname", nicknameText)
-            .get()
-            .addOnSuccessListener { documents ->
-                if (documents.isEmpty) {
-                    Log.d("!", "No duplicate found")
-                    checkDuplicateAccountInfoContinue()
+    private fun createAccount(email: String, password: String, nickname: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success
+                    showToast("Successfully created account")
+                    val user = auth.currentUser
+                    val uid: String = user!!.uid
+
+                    val currentUser = hashMapOf(
+                        "nickname" to nickname,
+                        "email" to email,
+                        "uid" to user.uid,
+                    )
+
+                    db.collection("userData")
+                        .document(uid).set(currentUser)
+                        .addOnSuccessListener {
+                            Log.d("!", "User added to Database with same UID as Firestore Auth ")
+                        }
+                        .addOnFailureListener { e ->
+                            Log.w("!", "Error adding document", e)
+                        }
+
+                    updateUI(user)
+
                 } else {
-                    Log.d("!", "Duplicate found!")
-                    showToast("Nickname already exists")
+                    // Sign in failed
+                    Log.w("!", "createUserWithEmail:failure", task.exception)
+                    showToast("Email already in use or badly written")
+                    updateUI(null)
                 }
             }
-            .addOnFailureListener { exception ->
-                Log.w("!", "Error getting documents: ", exception)
-            }
     }
 
-    private fun checkDuplicateAccountInfoContinue() { // Continuation of checkDuplicateAccountInfo function
-        db.collection("users")
-            .whereEqualTo("username", usernameText)
-            .get()
-            .addOnSuccessListener { documents ->
-                if (documents.isEmpty) {
-                    Log.d("!", "No duplicate found")
-                    createUserInFirebase()
-                } else {
-                    Log.d("!", "Duplicate found!")
-                    showToast("Username already exists")
-                }
-            }
-            .addOnFailureListener { exception ->
-                Log.d("!", "Error>> $exception")
-            }
-    }
+    private fun updateUI(user: FirebaseUser?) {
+        if (user != null) {
+            Log.d("!", "Account created / logged in")
+            Log.d("!", "----------------------------")
+            Log.d("!", "Nickname: $nickname")
+            Log.d("!", "Email: ${email}")
+            Log.d("!", "Uid: ${user.uid}")
 
-    private fun createUserInFirebase() {  // Adds the user inputs in database
-        val user = hashMapOf(
-            "nickname" to nicknameText,
-            "username" to usernameText,
-            "password" to passwordText,
-            "aboutMe" to aboutMeText
-            // To be added: phone number and confirmation code
-        )
+            val startHomeActivityIntent = Intent(this, HomeActivity::class.java)
+            startActivity(startHomeActivityIntent)
 
-        db.collection("users")
-            .add(user)
-            .addOnSuccessListener { documentReference ->
-                Log.d("!", "DocumentSnapshot added with ID: ${documentReference.id}")
-            }
-            .addOnFailureListener { e ->
-                Log.w("!", "Error adding document", e)
-            }
-
-        val createNewAccountActivity = Intent(this, HomeActivity::class.java)
-        startActivity(createNewAccountActivity)
+        } else Log.d("!", "User failed to log in")
     }
 
     private fun checkUserInputs(): Boolean {
-        nicknameText = createNicknameEditText.text.toString()
-        usernameText = createUsernameEditText.text.toString()
-        passwordText = createPasswordEditText.text.toString()
-        aboutMeText = createAboutMeEditText.text.toString()
-        phoneNumberText = createPhoneNumberEditText.toString()       // Phone number och Phone code
-        phoneCodeText = createConfirmPhoneNumberEditText.toString()  // används inte just nu
+        nickname = nicknameEditText.text.toString()
+        email = emailEditText.text.toString()
+        password = passwordEditText.text.toString()
 
-        return !(nicknameText.isEmpty() || usernameText.isEmpty() || passwordText.isEmpty() || aboutMeText.isEmpty())
+        return !(nickname.isEmpty() || email.isEmpty() || password.isEmpty())
     }
+
 
     fun showToast(toastMessage: String) {
         val toast = Toast.makeText(applicationContext, toastMessage, Toast.LENGTH_SHORT)
