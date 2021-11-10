668c99f5ca26aeed92ec5ceeb57a39e1d7fe790f
Mattias <Mattias170624@outlook.com>
2021-11-10 23:29:12 +0100

    Changed layout for all dialog fragments and added content

--- app/src/main/java/com/example/granne/CustomDialogFragment.kt
+++ app/src/main/java/com/example/granne/CustomDialogFragment.kt
@@ -1,7 +1,6 @@
 package com.example.granne
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,13 +11,8 @@ class CustomDialogFragment : DialogFragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        var rootView: View = inflater.inflate(R.layout.fragment_custom_dialog,container,false)
-
-        return rootView
+        savedInstanceState: Bundle?,
+    ): View {
+        return inflater.inflate(R.layout.fragment_custom_dialog, container, false)
     }
-
-
-
 }
