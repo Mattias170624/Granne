8984cf811e07b029a4d0358772def5bcd7e32662
Mattias <Mattias170624@outlook.com>
2021-11-11 17:52:10 +0100

    Added chat listener and layout for the chat

--- app/src/main/java/com/example/granne/ChatRoomActivity.kt
+++ app/src/main/java/com/example/granne/ChatRoomActivity.kt
@@ -3,6 +3,9 @@ package com.example.granne
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.CollectionReference
@@ -13,158 +16,125 @@ class ChatRoomActivity : AppCompatActivity() {
 
     data class Message(val name: String? = null, val text: String? = null)
 
+
     private lateinit var auth: FirebaseAuth
     private lateinit var myNickname: String
     val db = Firebase.firestore
 
     private lateinit var chatKey: String
     private lateinit var secondUserNickname: String
+    private lateinit var newMessageEditText: EditText
+    private lateinit var messageTextView: TextView
+    private lateinit var messageButton: Button
+    private lateinit var userTitle: TextView
+    lateinit var messageList: ArrayList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_room)
         auth = Firebase.auth
 
+        messageTextView = findViewById(R.id.messageTextView)
+        newMessageEditText = findViewById(R.id.newMessageEditText)
+        messageButton = findViewById(R.id.messageButton)
+        userTitle = findViewById(R.id.userTitle)
+
         secondUserNickname = intent.getStringExtra("secondUserNickname").toString()
         val secondUserUid: String = intent.getStringExtra("secondUserUid").toString()
-        chatKey = auth.currentUser!!.uid.plus(secondUserUid)
-
-        getChatChannel(chatKey)
-
-
-        //Log.d("!", "Chatting with: $secondUserNickname")
-        //Log.d("!", "The other persons uid: $secondUserUid")
-
-    }
-
-    private fun getChatChannel(chatKey: String) {
-        val allChatRefs = db.collection("chatRooms")
         val myDocRef = db.collection("userData").document(auth.currentUser!!.uid)
-        val txt = mutableListOf<String>()
-        txt.add("Heeej")
+
 
         myDocRef.get()
-            .addOnSuccessListener { documents ->
-                myNickname = documents.data!!.getValue("nickname").toString()
+            .addOnSuccessListener { name ->
+                myNickname = name.data!!.getValue("nickname").toString()
 
-                allChatRefs.get()
-                    .addOnSuccessListener { rooms ->
-
-                        for (chat in rooms) {
-                            when {
-                                !chat.exists() -> {
-                                    Log.d("!", "No key found but created one now")
-                                    updateUi(chatKey, false)
-                                }
-
-                                chat.exists() -> {
-                                    if (chat.id.contains(chatKey)) {
-                                        Log.d("!","Key found")
-                                        updateUi(chatKey, true)
-                                        break
-                                    }
-                                    if (!chat.id.contains(chatKey)) {
-                                        updateUi(chatKey, false)
-                                        break
-                                    }
-                                }
-                            }
-                        }
-
-                        /*
-                         else {
-
-                             // If chatRooms is filled with documents, get the document that contains the right chatKey
-                             for (room in rooms) {
-
-                                 if (room.id.contains(chatKey)) {
-                                     Log.d("!", "Chat exists!")
-                                     Log.d("!", "Texts inside this room: ${room.data}")
-
-
-                                     // If chatRooms does not contain our chatKey, then create a chat
-                                 } else {
-                                     Log.d("!", "No chatroom with this chatKey existed")
-                                     Log.d("!", "Creating one now with key: $chatKey")
-                                     getRoomContent(chatKey, myNickname)
-                                 }
-                             }
-                         }
-
-                         */
-
+                myDocRef.collection("matchedUsers").document(secondUserUid).get()
+                    .addOnSuccessListener { documents ->
+                        chatKey = documents.data!!.getValue("chatId").toString()
+                        createChatChannel(chatKey)
                     }
             }
+
     }
 
-    private fun updateUi(chatKey: String, exists: Boolean) {
-        val chatDocRef = db.collection("chatRooms")
+    private fun createChatChannel(chatKey: String) {
+        val chatDocRef = db.collection("chatRooms").document(chatKey)
+        messageList = arrayListOf()
+
         val chatInfo = hashMapOf(
-            "user" to secondUserNickname
+            "user1" to secondUserNickname,
+            "user2" to myNickname,
+            "messagelist" to messageList
         )
 
-        when {
-            !exists -> {
-                chatDocRef.document(chatKey).set(chatInfo)
-                    .addOnSuccessListener {
-                        Log.d("!", "Added new chat with user: $secondUserNickname")
-                        updateUi(chatKey, true)
-                    }
-            }
+        chatDocRef.get()
+            .addOnSuccessListener { task ->
+                if (!task.exists()) {
+                    Log.d("!", "No chat with the key: $chatKey")
 
-            exists -> {
-                chatDocRef.document(chatKey).get()
-                    .addOnSuccessListener { messages ->
-                        for (text in messages.data!!) {
-                            Log.d("!", "Texts in this chat: $text")
-
+                    chatDocRef.set(chatInfo)
+                        .addOnSuccessListener {
+                            Log.d("!", "Created chat with $secondUserNickname")
+                            updateChatUi()
                         }
-                    }
+                }
+                if (task.exists()) {
+                    Log.d("!", "Joining chat with $secondUserNickname with chatId: $chatKey")
+                    updateChatUi()
+                }
             }
-        }
+
     }
 
-    private fun getRoomContent(chatKey: String, myNickname: String) {
-        db.collection("chatRooms").document(chatKey)
-            .set(Message(myNickname, "Hej jag är snart en EditText"))
-            .addOnSuccessListener {
-                Log.d("!", "Created room content with a new message")
+    private fun updateChatUi() {
+        val chatDocRef = db.collection("chatRooms").document(chatKey)
+
+
+        // Sets title for the chatroom
+        val titleNickname = secondUserNickname
+        userTitle.text = titleNickname
+
+        chatDocRef.get()
+            .addOnSuccessListener { list ->
+                val oldList = list.data!!.getValue("messagelist").toString()
+                messageList.add(oldList)
+
+                messageButton.setOnClickListener {
+                    val text = "$myNickname: ${newMessageEditText.text}"
+                    messageList.add(text)
+                    newMessageEditText.text.clear()
+
+                    chatDocRef.update("messagelist", messageList)
+                }
             }
+
+        chatDocRef
+            .addSnapshotListener { snapshot, error ->
+                if (error != null) {
+                    return@addSnapshotListener // Stop listening to this snapshot
+                }
+
+                if (snapshot != null && snapshot.exists()) {
+                    val currentList = snapshot.data!!.getValue("messagelist")
+                    if (currentList.toString().isNotEmpty()) {
+                        messageTextView.text = currentList.toString()
+                            .replace("]", "")
+                            .replace("[", "")
+                            .replace(",","")
+                    }
+
+                    Log.d("!", "Before $messageList")
+                    //messageTextView.text = messageList.toString()
+                    Log.d("!", "AFter $messageList")
+                } else {
+                    Log.d("!", "Current data: null")
+                }
+
+            }
+
+
     }
 }
 
 
-/*
-    fun getRoomKey(secondUserUid: String, secondUserNickname: String, myNickname: String) {
-        val chatKey = auth.currentUser!!.uid.plus(secondUserUid)
-
-        db.collection("chatRooms").get()
-            .addOnSuccessListener { rooms ->
-                for (room in rooms) {
-                    if (room.contains(chatKey)) {
-                        //updateRoomWithMessages(chatKey, myNickname)
-                        val message = Message("myNickname", "Hejsaan")
-                        Log.d("!", "CHAT EXISTS with $secondUserNickname! $secondUserUid")
-                        Log.d("!", "Chats with this user: ${room.data}")
-                    } else {
-                        Log.d("!", "no foundo")
-                    }
-                }
-            }
-
-        val selectedChatRoom = db.collection("chatRooms").document()
-
-
-    }
-
-    fun updateRoomWithMessages(chatKey: String, myNickname: String) {
-        val message = Message(myNickname, "Hej")
-
-        db.collection("chatRooms").document(chatKey)
-            .set(message)
-            .addOnSuccessListener {
-                Log.d("!", "eee")
-            }
-    }
-*/
 
