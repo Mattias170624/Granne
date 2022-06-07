63b589111e122bf2b69e3a0871591d9f8a08d9db
rob <robsund@gmail.com>
2022-06-08 01:57:39 +0200

    naming in english

--- app/src/main/java/com/example/granne/PersonFindMatch.kt
+++ app/src/main/java/com/example/granne/PersonFindMatch.kt
@@ -3,7 +3,7 @@ package com.example.granne
 data class PersonFindMatch(
     val name: Any? = null,
     val aboutMe: Any? = null,
-    val intressen: MutableCollection<Any>? = null,
+    val interests: MutableCollection<Any>? = null,
     var uid: Any? = null,
     val buttonAdd: Any? = null,
     val image: Int? = R.drawable.userim,
